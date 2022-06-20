ad7678abac332d59fb849ed0a3d5fa94e01c87db
Theonom <theonomproduction@gmail.com>
2022-06-20 22:56:28 +0700

    Adding Asset In Play Scene & Input Paddle
    Membuat beberapa komponen aset didalam play scene dan membuat paddle controller

---
+++ 3D Pong/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public int speed;
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode rightKey;
+    public KeyCode leftKey;
+
+    private Rigidbody rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        MoveObject(GetInput());
+    }
+
+    private Vector3 GetInput()
+    {
+        if (Input.GetKey(upKey))
+        {
+            return Vector3.forward * speed;
+        }
+        if (Input.GetKey(downKey))
+        {
+            return Vector3.back * speed;
+        }
+        if (Input.GetKey(rightKey))
+        {
+            return Vector3.right * speed;
+        }
+        if (Input.GetKey(leftKey))
+        {
+            return Vector3.left * speed;
+        }
+
+        return Vector3.zero;
+    }
+
+    private void MoveObject(Vector3 movement)
+    {
+        rig.velocity = movement;
+    }
+}
